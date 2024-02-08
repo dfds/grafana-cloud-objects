caf12df07817d9f98cecde616ae35f60df01b949
Audun Nes <aunes@dfds.com>
2024-02-08 13:17:54 +0100

    use available metadata from dashboard module

--- main.tf
+++ main.tf
@@ -9,19 +9,19 @@ locals {
 
 module "ce_folder" {
   #checkov:skip=CKV_TF_1:We rely on release tags
-  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_folder?ref=0.3.1"
+  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_folder?ref=0.3.2"
   #source = "../../../../../../terraform-grafana-cloud//grafana_folder" # Support for local development
   title = var.folder_title
 }
 
 module "dashboards" {
   #checkov:skip=CKV_TF_1:We rely on release tags
-  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_dashboard?ref=0.3.1"
+  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_dashboard?ref=0.3.2"
   #source      = "../../../../../../terraform-grafana-cloud//grafana_dashboard" # Support for local development
   folder      = module.ce_folder.id
   config_json = local.json_data
 }
 
-output "dashboard_uid" {
-  value = module.dashboards.uid
+output "dashboard_meta" {
+  value = module.dashboards.meta
 }
