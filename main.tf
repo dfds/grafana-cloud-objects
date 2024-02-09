90e167b59b3ed063e0868bfb44c4d80f6fa21c1d
samidbb <samdi@dfds.com>
2024-02-09 14:11:11 +0100

    re-adding dashboard things

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-# locals {
-#   json_files = fileset(path.module, "dashboards/*.json")
-#   json_data = [for f in local.json_files : {
-#     filename = f
-#     content  = (file(f))
-#     }
-#   ]
-# }
+locals {
+  json_files = fileset(path.module, "dashboards/*.json")
+  json_data = [for f in local.json_files : {
+    filename = f
+    content  = (file(f))
+    }
+  ]
 
-locals { # For a single group
   alertrule_files = fileset(path.module, "alertrules/*.json")
 }
 
@@ -20,23 +18,23 @@ module "ce_folder" {
 
 
 
-# module "dashboards" {
-#   #checkov:skip=CKV_TF_1:We rely on release tags
-#   source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_dashboard?ref=0.3.2"
-#   #source      = "../../../../../../terraform-grafana-cloud//grafana_dashboard" # Support for local development
-#   folder      = module.ce_folder.id
-#   config_json = local.json_data
-# }
+module "dashboards" {
+  #checkov:skip=CKV_TF_1:We rely on release tags
+  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_dashboard?ref=0.3.2"
+  #source      = "../../../../../../terraform-grafana-cloud//grafana_dashboard" # Support for local development
+  folder      = module.ce_folder.id
+  config_json = local.json_data
+}
 
 
 module "alerts" {
   #checkov:skip=CKV_TF_1:We rely on release tags
-  # source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_dashboard?ref=0.3.2"
-  source          = "../terraform-grafana-cloud//grafana_alert" # Support for local development
+  # source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_alert?ref=0.3.2"
+  source          = "../../../../../../terraform-grafana-cloud//grafana_alert" # Support for local development
   folder          = module.ce_folder.uid
   alertrule_files = local.alertrule_files
 }
 
-# output "dashboard_meta" {
-#   value = module.dashboards.meta
-# }
+output "dashboard_meta" {
+  value = module.dashboards.meta
+}
