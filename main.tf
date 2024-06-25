54ec55a9f267f2c2e83e7d5460c29bef7104b30a
rifisdfds <rifis@dfds.com>
2024-06-25 13:20:51 +0100

    feat: increment grafana_notification

--- main.tf
+++ main.tf
@@ -50,12 +50,12 @@ module "grafana_data_source_aws_cloudwatch" {
 
 module "grafana_notification" {
   #checkov:skip=CKV_TF_1:We rely on release tags
-  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_notification?ref=0.19.1"
+  source = "git::https://github.com/dfds/terraform-grafana-cloud.git//grafana_notification?ref=0.21.0"
   #source               = "../../../../../../terraform-grafana-cloud//grafana_notification" # Support for local development
   notification_enabled = true
   name                 = "Cloud Engineering Slack"
   slack_webhook_url    = var.notification_slack_webhook_url
-  policy_enabled       = false
+  policy_enabled       = true
   policy_matcher = [{
     label = "grafana_folder"
     match = "="
